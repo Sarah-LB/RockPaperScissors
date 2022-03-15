29f36e7b0e1115fe3e6d566247eddae1a8f27a34
Sarah-LB <slloving@gmail.com>
2022-03-15 23:13:15 +0000

    add js functions to determine who wins

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//hover effect for selecting image
+//Hover effect for selecting image
 
 $(".play-image").mouseenter(function(){
     $(this).css({"width": "210px", "height": "210px"});
@@ -9,9 +9,10 @@ $(".play-image").mouseleave(function(){
 });
 
 
-//makes image of selected play appear in player box
+//Player's chosen symbol appears in player box
 
 $(".play-image").click(function(){
+
     if ($(this).attr("id")==="rock") {
         $("#player-move").css("background-image", "url('assets/images/rock.jpg')");
     } else if ($(this).attr("id")==="paper") {
@@ -19,19 +20,32 @@ $(".play-image").click(function(){
     } else if ($(this).attr("id")==="scissors") {
         $("#player-move").css("background-image", "url('assets/images/scissors.jpg')");
     } else {
-        console.log("none");
         $("#player-move").css("background-image", "none");
     }
     runGame();
 });
 
-//The main game "loop", called when the player chooses their move
+
 
 function runGame() {
 
+    //Player move is processed
+    var playerMove;
+
+    if ($("#player-move").css("background-image")==="rock") {
+        playerMove = "rock";
+    } else if ($("#player-move").css("background-image")==="paper") {
+        playerMove = "paper";
+    } else if ($("#player-move").css("background-image")==="scissors") {
+        playerMove = "scissors";
+    } else {
+        playerMove = "none");
+    }
+
+    //Computer move is generated
     symbols = ["rock", "paper", "scissors"]
     var computerMove = symbols[Math.floor(Math.random()*symbols.length)];
-    console.log(computerMove);
+    console.log(computerMove)
 
     if ($("#player-move").css("background-image")!=="none" && computerMove === "scissors") {
         $("#computer-move").css("background-image", "url('assets/images/scissors.jpg')");
@@ -43,9 +57,11 @@ function runGame() {
         $("#computer-move").css("background-image", "none");
     }
 
+    checkWhoWins(playerMove, computerMove);
+
 }
 
-/*const SYMBOL_CONFIG = [
+const SYMBOL_CONFIG = [
     {
         id: 'rock',
         name: 'Rock',
@@ -67,21 +83,21 @@ function runGame() {
 ];
 
 
-// takeUserChoice, getComputerChoice(randomly select any one), etc
+//Check who wins and increment scores
 
-function checkIfPlayerWins(playerChoice, computerChoice) {
+function checkWhoWins(playerChoice, computerChoice) {
 
     if(playerChoice === computerChoice) {
         // Show draw and do not increment any ones score
         return;
     }
-    // playerChoice -> rock, computerChoice -> paper
     const playerChoiceConfig = SYMBOL_CONFIG.find(eachSymbolConfig => eachSymbolConfig.id === playerChoice);
     if(playerChoiceConfig.winsOver === computerChoice) {
-        // Show player won
+        alert("You won! Keep going to increase your streak! :D");
         playerScore++;
     } else {
-         // Show computer won
+        alert("Oh no, you lost! Try again!");
          computerScore++;
     }
-}*/
+}
+
