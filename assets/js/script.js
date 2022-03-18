961b5873f28140102cb4fc6cd46305e5c299d0e2
Sarah-LB <slloving@gmail.com>
2022-03-18 22:33:42 +0000

    fixes the alert appearing before the images in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,61 +9,69 @@ $(".play-image").mouseleave(function(){
 });
 
 
-//Player's chosen symbol  and random computer symbol appear in play boxes
+//Player's chosen symbol appears in play boxes
 
 $(".play-image").click(function(){
+    
     var playerMove;
 
     if ($(this).attr("id")==="rock") {
         $("#player-move").css("background-image", "url('assets/images/rock.jpg')");
-        playerMove = 'rock';
+        playerMove = "rock";
     } else if ($(this).attr("id")==="paper") {
         $("#player-move").css("background-image", "url('assets/images/paper.jpg')");
-        playerMove = 'paper';
+        playerMove = "paper";
     } else if ($(this).attr("id")==="scissors") {
         $("#player-move").css("background-image", "url('assets/images/scissors.jpg')");
-        playerMove = 'scissors';
+        playerMove = "scissors";
     } else {
         playerMove = "none";
     }
-    
+
+    runGame(playerMove);
+});
+
+
+//Computer symbol generated
+
+function runGame(playerMove){
     symbols = ["rock", "paper", "scissors"]
     var computerMove = symbols[Math.floor(Math.random()*symbols.length)];
 
-    if ($("#player-move").css("background-image")!=="none" && computerMove === "scissors") {
+    if ($(this)!=="none" && computerMove === "scissors") {
         $("#computer-move").css("background-image", "url('assets/images/scissors.jpg')");
-    } else if ($("#player-move").css("background-image")!=="none" && computerMove === "rock") {
+    } else if ($(this)!=="none" && computerMove === "rock") {
         $("#computer-move").css("background-image", "url('assets/images/rock.jpg')");
-    } else if ($("#player-move").css("background-image")!=="none" && computerMove === "paper") {
+    } else if ($(this)!=="none" && computerMove === "paper") {
         $("#computer-move").css("background-image", "url('assets/images/paper.jpg')");
         return;
     }
 
     checkWhoWins(playerMove, computerMove);
 
-});
+};
 
 
 
 
 const SYMBOL_CONFIG = [
     {
-        id: 'rock',
-        name: 'Rock',
-        imgSrc: '<assets/images/rock.jpg>',
-        winsOver: 'scissors'
+        id: "rock",
+        name: "Rock",
+        imgSrc: "<assets/images/rock.jpg>",
+        winsOver: "scissors"
     },
     {
-        id: 'paper',
-        name: 'Paper',
-        imgSrc: '<assets/images/paper.jpg>',
-        winsOver: 'rock'
+        id: "paper",
+        name: "Paper",
+        imgSrc: "<assets/images/paper.jpg>",
+        winsOver: "rock"
     },
     {
-        id: 'scissors',
-        name: 'Scissors',
-        imgSrc: '<assets/images/scissors.jpg>',
-        winsOver: 'paper'
+        id: "scissors",
+        name: "Scissors",
+        imgSrc: "<assets/images/scissors.jpg>",
+        winsOver: "paper"
     }
 ];
 
@@ -72,22 +80,28 @@ const SYMBOL_CONFIG = [
 
 function checkWhoWins(playerChoice, computerChoice) {
 
-    let playerScore = parseInt(document.getElementById("player-score").innerHTML);
-    let computerScore = parseInt(document.getElementById("computer-score").innerHTML);
-    let currentStreak = parseInt(document.getElementById("current-streak").innerHTML);
+    let playerScore = parseInt(document.getElementById("player-score").innerText);
+    let computerScore = parseInt(document.getElementById("computer-score").innerText);
+    let currentStreak = parseInt(document.getElementById("current-streak").innerText);
 
     if(playerChoice === computerChoice) {
-        alert("It's a draw! Your streak continues!");
+        setTimeout(function(){
+            alert("It's a draw! Your streak continues!");
+        },10);
         currentStreak++;
         return;
     }
     const playerChoiceConfig = SYMBOL_CONFIG.find(eachSymbolConfig => eachSymbolConfig.id === playerChoice);
     if(playerChoiceConfig.winsOver === computerChoice) {
-        alert("You won! Keep going to increase your streak! :D");
+        setTimeout(function(){
+            alert("You won! Keep going to increase your streak! :D");
+        },10);
         playerScore++;
         currentStreak++;
     } else {
-        alert("Oh no, you lost! Keep going, try again!");
+        setTimeout(function(){
+            alert("Oh no, you lost! Keep going, try again!");
+        },10);
          computerScore++;
          currentStreak = 0;
     }
