2c1a4ba9243ab50a65cf8d35a17ddec14c59cd19
Sarah-LB <slloving@gmail.com>
2022-03-21 19:23:38 +0000

    add to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,7 +103,7 @@ $("#start-again").click(function () {
     $("#computer-move").css("background-image", "");
 });
 
-$("#start-small").click(function () {
+$("#restart").click(function () {
     document.getElementById("player-score").innerText = 0;
     document.getElementById("computer-score").innerText = 0;
     $("#player-move").css("background-image", "");
